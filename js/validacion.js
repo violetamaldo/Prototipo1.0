dfbfa9f9143a5a12e4c9ddd8f44cb8d41917e5a2
violetamaldo <violemmc@gmail.com>
2022-10-22 16:48:20 -0300

    Modificación validación
    formulario js

--- js/validacion.js
+++ js/validacion.js
@@ -1,5 +1,5 @@
 function validarContacto(){
-    //Variables
+    
     var formulario = document.getElementsByName("formulario")[0];
     console.log(formulario)
     var elementos = formulario.elements;
@@ -14,30 +14,30 @@ function validarContacto(){
 
     var validarApellidos = function(e){
         if (formulario.validarApellidos.value == 0){
-            alert("Completa el campo apellido");
+            alert("Completa el campo apellidos");
             e.preventDefault();
                     }
     };
 
-    var validarEmail = function(e){
+    var validarEmail = function(e) {
         if (formulario.validarEmail.value == 0){
-        } else {
             alert("Completa el campo e-mail");
             e.preventDefault();
-        }
+                    }
     };
     
-    var validarCheckbox = function(e){
-        if (formulario.terminos.checked == false){
-            alert("Acepta los términos y condiciones");
+    var validarMensaje = function(e){
+        if (formulario.validarMensaje.value == 0){
+            alert("Completa el campo mensaje");
             e.preventDefault();
-        }
+                     }
     };
 
     var validar = function(e){
         validarNombre(e);
-        validarRadio(e);
-        validarCheckbox(e);
+        validarApellidos(e);
+        validarEmail(e);
+        validarMensaje(e);
     };
     formulario.addEventListener("submit", validar);
     console.log(formulario) 
